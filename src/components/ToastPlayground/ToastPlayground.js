cecb0609fd0946e8183ae2b9d62eedc5d43ae7a6
jason-albers <soup@sandwich.net>
2025-06-16 19:38:29 -0500

    steps 1-3 complete

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import ToastShelf from '../ToastShelf/ToastShelf';
 import Button from '../Button';
 
 import styles from './ToastPlayground.module.css';
@@ -7,14 +7,51 @@ import styles from './ToastPlayground.module.css';
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
+  const [messageInput, setMessageInput] = React.useState('');
+  const [variantInput, setVariantInput] = React.useState('notice');
+  const [toastList, setToastList] = React.useState([]);
+  /*
+  verifying state is wired up:
+  console.log(messageInput);
+  console.log(variantInput);
+  console.log(toastList);
+  */
+
+  function handleSubmit() {
+    setToastList([
+      ...toastList,
+      {
+        id: `${
+          messageInput.slice(1, 6) +
+          Math.random() * 100 +
+          variantInput
+        }`,
+        message: messageInput,
+        variant: variantInput,
+      },
+    ]);
+    setMessageInput('');
+    setVariantInput('notice');
+  }
+
   return (
     <div className={styles.wrapper}>
       <header>
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
-
-      <div className={styles.controlsWrapper}>
+      <ToastShelf
+        variant={variantInput}
+        setToastList={setToastList}
+        toastList={toastList}
+      />
+      <form
+        className={styles.controlsWrapper}
+        onSubmit={(event) => {
+          event.preventDefault();
+          handleSubmit();
+        }}
+      >
         <div className={styles.row}>
           <label
             htmlFor="message"
@@ -24,27 +61,39 @@ function ToastPlayground() {
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea
+              id="message"
+              className={styles.messageInput}
+              value={messageInput}
+              onChange={(event) =>
+                setMessageInput(event.target.value)
+              }
+            />
           </div>
         </div>
 
         <div className={styles.row}>
           <div className={styles.label}>Variant</div>
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
-            <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-              />
-              notice
-            </label>
-
-            {/* TODO Other Variant radio buttons here */}
-          </div>
+          {VARIANT_OPTIONS.map((variant) => (
+            <div
+              className={`${styles.inputWrapper} ${styles.radioWrapper}`}
+              key={variant}
+            >
+              <label htmlFor={'variant-' + variant}>
+                <input
+                  id={'variant-' + variant}
+                  type="radio"
+                  name="variant"
+                  value={variant}
+                  checked={variantInput === variant}
+                  onChange={(event) =>
+                    setVariantInput(event.target.value)
+                  }
+                />
+                {variant}
+              </label>
+            </div>
+          ))}
         </div>
 
         <div className={styles.row}>
@@ -55,7 +104,7 @@ function ToastPlayground() {
             <Button>Pop Toast!</Button>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -3,15 +3,29 @@ import React from 'react';
 import Toast from '../Toast';
 import styles from './ToastShelf.module.css';
 
-function ToastShelf() {
+function ToastShelf({ toastList, setToastList }) {
+  function handleDismiss(id) {
+    const indexToDismiss = toastList.findIndex(
+      (item) => item.id === id
+    );
+    const newToastList = [...toastList];
+    newToastList.splice(indexToDismiss, 1);
+    setToastList(newToastList);
+  }
+
   return (
     <ol className={styles.wrapper}>
-      <li className={styles.toastWrapper}>
-        <Toast variant="notice">Example notice toast</Toast>
-      </li>
-      <li className={styles.toastWrapper}>
-        <Toast variant="error">Example error toast</Toast>
-      </li>
+      {toastList.map((toast) => (
+        <li className={styles.toastWrapper} key={toast.id}>
+          <Toast
+            variant={toast.variant}
+            handleDismiss={handleDismiss}
+            id={toast.id}
+          >
+            {toast.message}
+          </Toast>
+        </li>
+      ))}
     </ol>
   );
 }
